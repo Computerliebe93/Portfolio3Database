e480dd821ac8ed25a6b8dde97dbc1ad21517191e
Computerliebe93 <70689092+Computerliebe93@users.noreply.github.com>
2020-11-20 00:56:49 +0100

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import static java.sql.DriverManager.getConnection;
 
 public class Main {
 
@@ -13,25 +8,19 @@ public class Main {
             String url = "jdbc:sqlite:/Users/martinemildaafunder/Desktop/SD/Portfolio3Database/Untitled";
             poodleDB = new PoodleDB();
             poodleDB.connect(url);
-            // Statement
-            String sql;
-            sql = "SELECT * FROM students";
-            ResultSet rs = poodleDB.sqlStatement(sql);
-            rs = poodleDB.sqlPreparedStatement("Theis Thomasen", "SD", 10);
 
         }catch (SQLException e) {
             e.printStackTrace();
         }
         finally {
-            if ( poodleDB.conn != null)
+            if (poodleDB.conn != null)
             {
-                try {
+                try{
                     poodleDB.conn.close();
                 }catch (SQLException d){
                     d.printStackTrace();
                 }
             }
         }
-
     }
 }
--- src/PoodleDB.java
+++ src/PoodleDB.java
@@ -10,36 +10,4 @@ public class PoodleDB {
     public void closeConnection() throws SQLException {
         conn.close();
     }
-
-    public ResultSet sqlStatement(String sql) throws SQLException {
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
-        if (rs == null)
-            System.out.println();
-        System.out.println("No record retrieved");
-        while (rs != null & rs.next()) {
-            String Name = rs.getString("City");
-            System.out.println(Name);
-        }
-        return rs;
-    }
-    public ResultSet sqlPreparedStatement(String StudentName, String Course, int Grade) throws SQLException {
-        String sql = " SELECT D1.StudentName as Student, D2.StudentName as exam, D2.Grade as Grade " +
-        " FROM sdExam as D1 " +
-        " JOIN sdExam as D2 " +
-        " JOIN sdExam as D2 on D1.StudentName = D2.StudentName " +
-       " WHERE D1.StudentName = ? and D2.sdExam = ? and D2.Grade = ?;";
-        PreparedStatement pstmt = conn.prepareCall(sql);
-        pstmt.setString(1, StudentName);
-        pstmt.setString(2, Course);
-        pstmt.setInt(3, Grade);
-        ResultSet rs = pstmt.executeQuery();
-        if (rs == null)
-            System.out.println();
-        System.out.println("No record retrieved");
-        while (rs != null & rs.next()) {
-            System.out.println(rs.getString(1 + " " + rs.getInt(2) + " " + rs.getString(3)));
-        }
-        return rs;
-    }
 }
